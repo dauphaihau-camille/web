22b3d800e22af494168b47ea4f2a94fb509e5222
hau <dauphaihau@gmail.com>
2026-06-28 14:39:58 +0700

    chore(web): migrate linting and reformat app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from 'next/font/google';
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets:['latin'],variable:'--font-sans' });
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
 
 export default function RootLayout({
   children,
@@ -20,11 +20,11 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn('antialiased', fontMono.variable, 'font-sans', geist.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import dauphaihauConfig from '@dauphaihau/eslint-config';
+import eslintNextPlugin from '@next/eslint-plugin-next';
+import { defineConfig } from 'eslint/config';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+const nextRules = {
+  ...eslintNextPlugin.configs.recommended.rules,
+  ...eslintNextPlugin.configs['core-web-vitals'].rules,
+};
+
+export default defineConfig([
+  ...(await dauphaihauConfig()),
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': eslintNextPlugin,
+    },
+    rules: nextRules,
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+  {
+    ignores: ['out/**', 'next-env.d.ts'],
+  },
 ]);
-
-export default eslintConfig;
