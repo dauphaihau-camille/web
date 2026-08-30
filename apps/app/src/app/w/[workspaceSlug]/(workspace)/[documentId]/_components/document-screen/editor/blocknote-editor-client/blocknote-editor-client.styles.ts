939a0351c4f251c364f65d556666ff016ec456c3
hau <dauphaihau@gmail.com>
2026-08-30 14:44:50 +0700

    fix(document-editor): tighten bullet marker spacing

--- apps/app/src/app/w/[workspaceSlug]/(workspace)/[documentId]/_components/document-screen/editor/blocknote-editor-client/blocknote-editor-client.styles.ts
+++ apps/app/src/app/w/[workspaceSlug]/(workspace)/[documentId]/_components/document-screen/editor/blocknote-editor-client/blocknote-editor-client.styles.ts
@@ -3,6 +3,19 @@ import { blockNoteEditorClientBaseStyles } from '@shared/components/editor/block
 export const blockNoteEditorClientStyles = `
 ${blockNoteEditorClientBaseStyles}
 
+  .editor-blocknote-client .bn-block-content[data-content-type="bulletListItem"]::before,
+  .editor-blocknote-client .bn-block-content[data-content-type="numberedListItem"]::before {
+    min-width: 0.875rem;
+    padding-right: 0;
+    padding-left: 0.9rem;
+  }
+
+  .editor-blocknote-client
+    .bn-block-content:is([data-content-type="bulletListItem"], [data-content-type="numberedListItem"])
+    > .bn-inline-content {
+    padding-left: 0.5rem;
+  }
+
   .editor-blocknote-client .bn-formatting-toolbar:empty {
     display: none !important;
   }
